c8dc1b3760fa22700c98b555a5b33008cd8a471f
AnnDutova <ann.dutova@list.ru>
2024-09-21 23:16:39 +0300

    HW11: complited

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,48 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"time"
+)
+
+var timeout time.Duration
+
+func init() {
+	flag.DurationVar(&timeout, "timeout", 10*time.Second, "telnet client default timeout")
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	flag.Parse()
+
+	if len(os.Args[2:]) < 2 {
+		fmt.Println("Invalid argument count")
+		os.Exit(1)
+	}
+
+	addr := net.JoinHostPort(os.Args[2], os.Args[3])
+	tc := NewTelnetClient(addr, timeout, os.Stdin, os.Stdout)
+
+	if err := tc.Connect(); err != nil {
+		fmt.Printf("Error occurred during creation Telnet client connection, err: %v", err)
+		os.Exit(1)
+	}
+	defer tc.Close()
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	go func() {
+		tc.Receive()
+	}()
+
+	go func() {
+		tc.Send()
+	}()
+
+	<-ctx.Done()
 }
